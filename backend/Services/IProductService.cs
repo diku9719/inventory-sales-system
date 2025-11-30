6cc46f538aec80ff61b15119f7bcfa9ef5290f2b
diku9719 <91065725+diku9719@users.noreply.github.com>
2025-11-30 19:22:55 +0530

    Add IProductService interface

---
+++ backend/Services/IProductService.cs
@@ -0,0 +1,14 @@
+using InventorySystem.Models;
+
+namespace InventorySystem.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<Product?> GetProductByIdAsync(int id);
+        Task<IEnumerable<Product>> GetLowStockProductsAsync();
+        Task<Product> CreateProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(int id);
+    }
+}
